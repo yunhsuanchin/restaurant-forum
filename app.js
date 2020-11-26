1b1d0338df0d038be5a84341c74dfff3f6efa475
yunhsuanchin <alicechin20181@gmail.com>
2020-11-26 15:29:27 +0800

    Feat: initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const db = require('./models')
 const app = express()
 const port = 3000
 
@@ -9,6 +10,7 @@ app.set('view engine', 'hbs')
 require('./routes')(app)
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
