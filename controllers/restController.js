b8fc3b18aacb3390df65c4d07e18b4a1f571b8ac
yunhsuanchin <alicechin20181@gmail.com>
2020-11-30 23:08:32 +0800

    Feat: add restaurant index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,20 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: async (req, res) => {
+    try {
+      const data = await Restaurant.findAll({ include: Category, raw: true, nest: true })
+      const restaurants = data.map((item) => ({
+        ...item,
+        categoryName: item.Category.name,
+        description: item.description.substring(0, 50)
+      }))
+      res.render('restaurants', { restaurants })
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
