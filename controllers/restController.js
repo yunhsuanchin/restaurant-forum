14876fa35e3f68a72009d61a0984de95008f7dbd
yunhsuanchin <alicechin20181@gmail.com>
2020-11-30 23:47:56 +0800

    Feat: add category filter in restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -5,13 +5,21 @@ const Category = db.Category
 const restController = {
   getRestaurants: async (req, res) => {
     try {
-      const data = await Restaurant.findAll({ include: Category, raw: true, nest: true })
+      const whereQuery = {}
+      const categoryId = Number(req.query.categoryId)
+      console.log('categoryId', categoryId)
+      if (categoryId) {
+        console.log('categoryId', categoryId)
+        whereQuery.categoryId = categoryId
+      }
+      const data = await Restaurant.findAll({ include: Category, raw: true, nest: true, where: whereQuery })
       const restaurants = data.map((item) => ({
         ...item,
         categoryName: item.Category.name,
         description: item.description.substring(0, 50)
       }))
-      res.render('restaurants', { restaurants })
+      const categories = await Category.findAll({ raw: true, nest: true })
+      res.render('restaurants', { restaurants, categories, categoryId })
     } catch (error) {
       console.log(error)
     }
