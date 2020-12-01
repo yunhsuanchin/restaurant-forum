0a1e5a7e0f59cf7048a55e37afeb9fa9852f4226
yunhsuanchin <alicechin20181@gmail.com>
2020-12-01 17:21:38 +0800

    Feat: add paginator

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,25 +1,51 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const limit = 12
 
 const restController = {
   getRestaurants: async (req, res) => {
     try {
-      const whereQuery = {}
+      let offset = 0
+      const page = Number(req.query.page) || 1
       const categoryId = Number(req.query.categoryId)
-      console.log('categoryId', categoryId)
+      const whereQuery = {}
+
+      if (page) {
+        offset = (page - 1) * limit
+      }
       if (categoryId) {
-        console.log('categoryId', categoryId)
         whereQuery.categoryId = categoryId
       }
-      const data = await Restaurant.findAll({ include: Category, raw: true, nest: true, where: whereQuery })
-      const restaurants = data.map((item) => ({
+      const data = await Restaurant.findAndCountAll({
+        include: Category,
+        raw: true,
+        nest: true,
+        where: whereQuery,
+        offset,
+        limit
+      })
+      const totalPage = Math.ceil(data.count / limit)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > totalPage ? totalPage : page + 1
+      const pages = Array.from(Array(totalPage)).map((item, index) => index + 1)
+
+      const restaurants = data.rows.map((item) => ({
         ...item,
         categoryName: item.Category.name,
         description: item.description.substring(0, 50)
       }))
       const categories = await Category.findAll({ raw: true, nest: true })
-      res.render('restaurants', { restaurants, categories, categoryId })
+      res.render('restaurants', {
+        restaurants,
+        categories,
+        categoryId,
+        totalPage,
+        pages,
+        page,
+        prev,
+        next
+      })
     } catch (error) {
       console.log(error)
     }
