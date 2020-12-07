7d28f8e5c27158d4a44dc63aaf09e9497bc5c45d
yunhsuanchin <alicechin20181@gmail.com>
2020-12-07 17:41:49 +0800

    Refactor: modify api/adminController.js

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -1,18 +1,14 @@
 const db = require('../../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const adminService = require('../../services/adminService')
+
 const adminController = {
-  getRestaurants: async (req, res) => {
-    try {
-      const restaurants = await Restaurant.findAll({
-        aw: true,
-        nest: true,
-        include: Category
-      })
-      res.json({ restaurants })
-    } catch (error) {
-      console.log(error)
-    }
+  getRestaurants: (req, res) => {
+    adminService.getRestaurants(req, res, (data) => {
+      return res.json(data)
+    })
   }
 }
+
 module.exports = adminController
